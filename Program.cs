65df83c60e7135286b2984679f84534826643c5e
OlegSs <youremail@yourdomain.comolegs934@gmail.com>
2022-10-13 15:14:23 +0300

    Задача решена

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите цикл, который принимает на вход два числа A и B и возводит число A в натуральную степень B
+// 3, 5 -> 243
+// 2, 4 -> 14
+
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+int degr = A;
+
+for (int i = 1; i < B; i++)
+{
+degr = degr * A;
+}
+Console.WriteLine("A в степени B = " + degr);
+
